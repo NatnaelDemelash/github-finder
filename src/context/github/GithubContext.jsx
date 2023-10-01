1aa19a2ea78026660f58434c2b912a0fd5d86e85
Natnael Demelash <natidemelash074@gmail.com>
2023-10-01 11:27:16 +0300

    Shifted from useState to useReducer

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,25 +1,32 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./githubReducer";
 
 const GithubContext = createContext();
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const initialState = {
+    user: [],
+    isLoading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch("https://api.github.com/users");
     const data = await response.json();
 
-    setUsers(data);
-    setIsLoading(false);
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        isLoading,
+        users: state.users,
+        isLoading: state.isLoading,
         fetchUsers,
       }}
     >
